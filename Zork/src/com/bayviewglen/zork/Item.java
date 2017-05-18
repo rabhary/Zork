000d78a47cd17b8a16d801b78024e0e7af47d812
smaxwell <smaxwell@smaxwellsp3.bayviewglen.ca>
2017-05-18 11:11:04 -0400

    Added Items and Inventory

---
+++ Zork/src/com/bayviewglen/zork/Item.java
@@ -0,0 +1,36 @@
+package com.bayviewglen.zork;
+
+public class Item {
+
+	
+
+		private String description;
+		private int mass;
+
+		//create getters and setters
+		public String getDescription() {
+			return description;
+		}
+		public void setDescription(String description) {
+			this.description = description;
+		}
+		public int getMass() {
+			return mass;
+		}
+		public void setMass(int mass) {
+			this.mass = mass;
+		}
+
+		public Item(String description, int mass) {
+			super();
+			this.description = description;
+			this.mass = mass;
+		}
+		public Item(String description) {
+			super();
+			this.description = description;
+			this.mass = 0;
+		}
+	
+
+}
